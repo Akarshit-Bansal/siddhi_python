83b306357fc24265bc51940e38d49cbb1fe9cf78
Akarshit Bansal <bansalakarshit@gmail.com>
2026-06-16 12:09:26 +0530

    Fix static files

--- siddhi/settings.py
+++ siddhi/settings.py
@@ -66,5 +66,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'main/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
