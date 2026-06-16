f751e7af279cbaf440877bbc215bf29993dace7c
Akarshit Bansal <bansalakarshit@gmail.com>
2026-06-16 15:50:55 +0530

    Remove manifest storage

--- siddhi/settings.py
+++ siddhi/settings.py
@@ -72,4 +72,4 @@ STATICFILES_DIRS = [BASE_DIR / 'main/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
