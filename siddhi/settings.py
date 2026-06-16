b2195da18088f275a00b5485caaa3214a79dbac3
Akarshit Bansal <bansalakarshit@gmail.com>
2026-06-16 15:32:31 +0530

    Configure WhiteNoise

--- siddhi/settings.py
+++ siddhi/settings.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -69,3 +72,4 @@ STATICFILES_DIRS = [BASE_DIR / 'main/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
